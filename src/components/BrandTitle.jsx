20cccb80e8e16e8344ff4e770eafa8e497f0a750
mertozkar <mertozkar@gmail.com>
2022-11-18 10:32:33 +0300

    update

--- src/components/BrandTitle.jsx
+++ src/components/BrandTitle.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Button, Col, Row, Label, Input } from 'reactstrap';
+import { Button, Col, Row } from 'reactstrap';
+import db from '../firebase';
 
 const BrandTitle = ({ title, id, author, editor, startDate, endDate, status }) => {
 
@@ -14,49 +15,51 @@ const BrandTitle = ({ title, id, author, editor, startDate, endDate, status }) =
 
     const removeTitleFromDb_ = (id) => {
         console.log(localStorage.getItem("brandId") + " " + id + " ")
-        //db.collection("brands").doc(localStorage.getItem("brandId")).collection("titles").doc(id).delete();
-        //setTph(current => !current)
+        db.collection("brands").doc(localStorage.getItem("brandId")).collection("titles").doc(id).delete();
+        setTph(current => !current)
     }
 
     return (
-        <tr key={id} className="brand-title-table">
-            <td>{title}</td>
-            <td>{author}</td>
-            <td>{editor}</td>
-            <td>{startDate}</td>
-            <td>{endDate}</td>
-            <td>{status}</td>
-            <td><button type="button" class="btn btn-danger m-2" onClick={(e) => removeTitleFromDb(id)}>Sil</button></td>
-            {tph && <div className="popup-box">
-                <div className="box">
-                    <span className="close-icon" onClick={toggleHandle}>x</span>
-                    <Row className="product-name"> Başlığı silmek istediğinize emin misiniz?</Row>
-                    <Row>
-                        <Col>
-                            <Button
-                                color="success"
-                                outline
-                                className="float-end"
-                                onClick={(e) => removeTitleFromDb_(id)}
-                            >
-                                Sil
-                            </Button>
-                        </Col>
-                        <Col>
-                            <Button
-                                color="danger"
-                                outline
-                                onClick={toggleHandle}
-                            >
-                                İptal
-                            </Button>
-                        </Col>
-                    </Row>
+        <>
+            <tr key={id} className="brand-title-table">
+                <td>{title}</td>
+                <td>{author}</td>
+                <td>{editor}</td>
+                <td>{startDate}</td>
+                <td>{endDate}</td>
+                <td>{status}</td>
+                <td><button type="button" class="btn btn-danger m-2" onClick={(e) => removeTitleFromDb(id)}>Sil</button></td>
+                <td><button type="button" class="btn btn-warning m-2">Güncelle</button></td>
+                {tph && <div className="popup-box">
+                    <div className="box">
+                        <span className="close-icon" onClick={toggleHandle}>x</span>
+                        <Row className="product-name"> Başlığı silmek istediğinize emin misiniz?</Row>
+                        <Row>
+                            <Col>
+                                <Button
+                                    color="success"
+                                    outline
+                                    className="float-end"
+                                    onClick={(e) => removeTitleFromDb_(id)}
+                                >
+                                    Sil
+                                </Button>
+                            </Col>
+                            <Col>
+                                <Button
+                                    color="danger"
+                                    outline
+                                    onClick={toggleHandle}
+                                >
+                                    İptal
+                                </Button>
+                            </Col>
+                        </Row>
 
-                </div>
-            </div>}
-            <td><button type="button" class="btn btn-warning m-2">Güncelle</button></td>
-        </tr>
+                    </div>
+                </div>}
+            </tr>
+        </>
     )
 }
 
